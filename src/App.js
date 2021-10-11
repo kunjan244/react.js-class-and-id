b6bb3c6e5ea18ea5ebca45a23ea5f511b99c49d2
kunjan244 <kunjanavghane@gmail.com>
2021-10-11 16:25:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Header from './component/Header';
+import Section from './component/Section';
+import Footer from './component/Footer';
+
+
+
+
+
+
+
+function App() {
+  return (
+    <div>
+      <h1>This is default Component</h1>
+    <Header />
+    <hr />
+    <Section />
+    <hr />
+    <Footer />
+    <hr />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+class Footer extends React.Component{
+    render(){
+return<h1>This is class Footer Component</h1>
+    }
+}
+export default Footer;
